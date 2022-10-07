0330452c1970504b0226e026ca0a99b878404b27
jedad <ad.jedrzejczyk2@gmail.com>
2022-10-08 01:26:20 +0200

    App Dzienniczek działa

--- src/gradebook/Program.cs
+++ src/gradebook/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// using System.Collections.Generic;
 
 namespace gradebook // Note: actual namespace depends on the project name.
 {
@@ -6,7 +7,17 @@ namespace gradebook // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Hello {args[0]}!");
+            var book = new Book("Dzienniczek Marysi");
+            var grades = new List<double>() {12.3, 34.5, 56.7};
+            grades.Add(11.4);
+
+            var result = 0.0;
+            foreach(var grade in grades)
+            {
+                result += grade;
+            } 
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
         }
     }
 }
